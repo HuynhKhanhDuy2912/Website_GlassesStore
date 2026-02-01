98532c4469f69e84560741bebbbeeba107c4982c
HuynhKhanhDuy2912 <duy2912www@gmail.com>
2026-02-01 17:42:36 +0700

    adjust template

--- frontend/src/pages/OrderDetailPage.jsx
+++ frontend/src/pages/OrderDetailPage.jsx
@@ -117,11 +117,8 @@ const OrderDetailPage = () => {
 
   const statusConfig = {
     pending: { label: "ĐANG XỬ LÝ", color: "bg-yellow-100 text-yellow-800" },
-    confirmed: { label: "ĐÃ XÁC NHẬN", color: "bg-blue-100 text-blue-800" },
-    delivered: {
-      label: "ĐANG GIAO HÀNG",
-      color: "bg-indigo-100 text-indigo-800",
-    },
+    confirmed: { label: "ĐÃ XÁC NHẬN", color: "bg-blue-100 text-orange-800" },
+    delivered: { label: "ĐANG GIAO HÀNG", color: "bg-indigo-100 text-indigo-800"},
     completed: { label: "HOÀN THÀNH", color: "bg-green-100 text-green-700" },
     cancelled: { label: "ĐÃ HỦY", color: "bg-red-100 text-red-700" },
   };
